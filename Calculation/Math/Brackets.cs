35ab6ecf55ee5981fe8c3648c4165b867ebededc
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:31:02 +0300

    Add project files.

---
+++ Calculation/Math/Brackets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculation.Math
+{
+    public class Brackets
+    {
+        public Brackets()
+        {
+
+        }
+
+        public int CheckSymbols(char[] symbols)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Calculation/Math/CalculationQueue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation.Math
+{
+    public class CalculationQueue
+    {
+        private List<Brackets> bracketsList;
+
+        public CalculationQueue(string expression)
+        {
+            bracketsList = new List<Brackets>();
+
+            char[] expressionSymbols = expression.ToCharArray();
+            for (int i = 0; i < expressionSymbols.Length; i++)
+            {
+                if (expressionSymbols[i].Equals('('))
+                {
+                    char[] symbols = new char[expressionSymbols.Length - i];
+                    for (int j = 0; j < symbols.Length; j++)
+                        symbols[j] = expressionSymbols[i + j];
+                    Brackets brackets = new Brackets();
+                    i += brackets.CheckSymbols(symbols);
+                    if (i == -1)
+                        return;
+                }
+            }
+        }
+    }
+}
